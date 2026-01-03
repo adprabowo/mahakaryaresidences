78e6fc8da9f09a51523c288341a7de835dfd9098
Aditya Prabowo <adityaprabowo@Adityas-MacBook-Pro.local>
2026-01-03 18:22:16 +0700

    update map error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -587,7 +587,7 @@ const MahakaryaLanding = () => {
                   width="100%" 
                   height="100%" 
                   style={{ border: 0 }}
-                  src="https://googleusercontent.com/maps.google.com/20"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.2017160053824!2d103.54515417479128!3d-1.6310887983537428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e258762995df6a1%3A0x324e4820b8d78e91!2sPerumahan%20Mahakarya%20Residence!5e0!3m2!1sid!2sid!4v1767439233439!5m2!1sid!2sid"
                   className="filter grayscale hover:grayscale-0 transition duration-500"
                   allowFullScreen
                   loading="lazy" 
@@ -595,7 +595,7 @@ const MahakaryaLanding = () => {
                 ></iframe>
               </div>
               <a 
-                href="https://goo.gl/maps/6qX4X5X4X5X4X5X4X" 
+                href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.2017160053824!2d103.54515417479128!3d-1.6310887983537428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e258762995df6a1%3A0x324e4820b8d78e91!2sPerumahan%20Mahakarya%20Residence!5e0!3m2!1sid!2sid!4v1767439233439!5m2!1sid!2sid" 
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-xs text-[#D94116] hover:text-white mt-2 inline-block transition"
